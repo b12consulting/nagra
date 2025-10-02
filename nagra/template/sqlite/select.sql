cd47bb8a7a7b8f516b3f218c7f04f1f694ea2804
Sebastien Wertz <sebastien.wertz@b12-consulting.com>
2025-10-02 14:04:42 +0200

    Fix select templates for joins on foreign keys in titlecase

--- nagra/template/sqlite/select.sql
+++ nagra/template/sqlite/select.sql
@@ -3,8 +3,8 @@ SELECT
 FROM "{{table}}"
 
 {%- for next_table, alias, prev_table, alias_col, prev_col in joins %}
- LEFT JOIN "{{next_table}}" as {{alias}} ON (
-    {{alias}}."{{alias_col}}" = "{{prev_table}}"."{{prev_col}}"
+ LEFT JOIN "{{next_table}}" as "{{alias}}" ON (
+    "{{alias}}"."{{alias_col}}" = "{{prev_table}}"."{{prev_col}}"
  )
 {%- endfor -%}
 
