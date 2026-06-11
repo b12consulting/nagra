b9b9754abe99f0533476b6feb82272c093cb78cc
Bertrand Chenal <bertrand.chenal@b12-consulting.com>
2026-06-11 14:02:29 +0200

    Use a connection pool with postgres

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -5,6 +5,9 @@ from nagra.exceptions import NoActiveTransaction, TransactionReenterError
 from nagra.schema import Schema
 
 
+POSTGRESQL_DSN = "postgresql:///nagra"
+
+
 def test_dummy_transaction():
     with pytest.raises(NoActiveTransaction):
         dummy_transaction.execute("SELECT 1")
@@ -15,22 +18,20 @@ def test_dummy_transaction():
 
 def test_transaction_reuse():
     with pytest.raises(TransactionReenterError):
-        trn = Transaction("postgresql:///nagra")
+        trn = Transaction(POSTGRESQL_DSN)
         with trn:
             with trn:
                 pass
 
 
 def test_concurrent_transaction(person, schema: Schema):
-    uri = "postgresql:///nagra"
-
-    with Transaction(uri):
+    with Transaction(POSTGRESQL_DSN):
         schema.create_tables()
         # Cleanup
         person.delete()
 
-    trn_a = Transaction(uri)
-    trn_b = Transaction(uri)
+    trn_a = Transaction(POSTGRESQL_DSN)
+    trn_b = Transaction(POSTGRESQL_DSN)
 
     person.upsert("name", trn=trn_a).execute("Romeo")
     person.upsert("name", trn=trn_b).execute("Sierra")
@@ -38,7 +39,7 @@ def test_concurrent_transaction(person, schema: Schema):
     trn_a.commit()
     trn_b.rollback()
 
-    with Transaction(uri) as tr:
+    with Transaction(POSTGRESQL_DSN) as tr:
         records = list(person.select("name"))
         assert records == [("Romeo",)]
 
@@ -47,3 +48,65 @@ def test_concurrent_transaction(person, schema: Schema):
             if tbl.is_view:
                 continue
             tr.execute(f"DROP TABLE {tbl.name} CASCADE")
+
+
+def test_postgresql_connection_pool_is_cached_by_dsn():
+    Transaction.shutdown_pools()
+    try:
+        trn_a = Transaction(POSTGRESQL_DSN)
+        trn_b = Transaction(POSTGRESQL_DSN)
+
+        assert trn_a._pool is trn_b._pool
+        assert Transaction._pool_cache[POSTGRESQL_DSN] is trn_a._pool
+        assert trn_a._connection is None
+        assert trn_b._connection is None
+    finally:
+        Transaction.shutdown_pools()
+
+
+def test_postgresql_connection_is_borrowed_lazily_and_returned_on_close():
+    Transaction.shutdown_pools()
+    try:
+        trn = Transaction(POSTGRESQL_DSN)
+        assert trn._connection is None
+
+        connection = trn.connection
+        assert connection is not None
+        assert trn.connection is connection
+
+        pool = trn._pool
+        trn.close()
+
+        assert trn._connection is None
+
+        reused_trn = Transaction(POSTGRESQL_DSN)
+        assert reused_trn._pool is pool
+        assert reused_trn.connection is connection
+        reused_trn.close()
+    finally:
+        Transaction.shutdown_pools()
+
+
+def test_postgresql_close_without_borrowed_connection_does_not_touch_pool():
+    Transaction.shutdown_pools()
+    try:
+        trn = Transaction(POSTGRESQL_DSN)
+        pool = trn._pool
+
+        trn.close()
+
+        assert trn._connection is None
+        assert Transaction._pool_cache[POSTGRESQL_DSN] is pool
+    finally:
+        Transaction.shutdown_pools()
+
+
+def test_shutdown_pools_closes_and_clears_postgresql_pools():
+    Transaction.shutdown_pools()
+    Transaction(POSTGRESQL_DSN)
+
+    assert POSTGRESQL_DSN in Transaction._pool_cache
+
+    Transaction.shutdown_pools()
+
+    assert Transaction._pool_cache == {}
